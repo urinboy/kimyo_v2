5c354709a45796eccd0e49255ebd08e1a053a151
urinboy <urinboytursunboev@gmail.com>
2026-06-03 20:01:35 +0500

    feat: import missing lesson lab items and task submission data via SQL scripts

---
+++ lesson_translations_update.sql
@@ -0,0 +1,602 @@
+-- lesson_translations content update (27 ta)
+UPDATE lesson_translations SET title='Atom tuzilishi', content='## Atom tuzilishi - kimyoviy elementning barcha xossalarini oʻzida saqlab qoluvchi eng kichik zarrachasidir.
+
+## Atom tarkibi:
+
+## \-Yadro- markazda joylashgan
+
+## \-Protonlar (+) zaryadli
+
+## \-Neytronlar (0) zaryadli
+
+## Elektron qobig''i
+
+## \-Elektronlar (-) zaryadli
+
+## Yadro atrofida harakatlanadi', updated_at='2026-05-12 05:45:15' WHERE id=1;
+UPDATE lesson_translations SET title='Строение атома', content='*   **Строение атома это наименьшая частица химического элемента, сохраняющая в себе все его свойства.**
+    
+*   **Атомный состав:**
+    
+*   **Ядро расположено в центре**
+    
+*   **Протоны имеют заряд (+).**
+    
+*   **Нейтроны (0) заряжены.**
+    
+*   **Электронная оболочка**
+    
+*   **Электроны (-) заряжены.**', updated_at='2026-05-12 05:30:34' WHERE id=2;
+UPDATE lesson_translations SET title='Atomic structure', content='Atomic structure is the smallest particle that preserves all the properties of a chemical element.
+
+Atomic composition:
+
+The nucleus is located in the center
+
+Protons with (+) charge
+
+Neutrons (0) charged
+
+Electron shell
+
+Electrons (-) charged
+
+Moves around the nucleus', updated_at='2026-05-12 05:43:54' WHERE id=3;
+UPDATE lesson_translations SET title='Kimyoviy bog''lanish', content='*   Kimyoviy bog''lanish
+    
+*   Atomlar orasida bog'' hosil bolish usullari:
+    
+*   1\. Kovalant bog''lanish
+    
+*   \-Elektronlar umumiy
+    
+*   \-Nometallar orasida
+    
+*   \-Misol: H2, O2, H2O
+    
+*   2\. Ion bog''lanish - Elektronlar o''rtadi
+    
+*   \- Metall va nometallar
+    
+*   Misol: NaCL, MgO
+    
+*   3\. Metall bog''lanishi
+    
+*   \-Elektronlar erkin harakatda
+    
+*   \-Metallar orasida
+    
+*   Misol: Fe, Cu, Al
+    
+*   4\. Vododrod bog''lanishi
+    
+*   \- Zaif bog''
+    
+*   \- Molekulalar orasida
+    
+*   \-Misol Suvda, DNK da', updated_at='2026-05-12 06:03:57' WHERE id=5;
+UPDATE lesson_translations SET title='Химическая связь', content='Способы образования связей между атомами:
+
+1\. Ковалентная связь.
+
+\-Электроны общие
+
+\- Среди неметаллических
+
+\-Пример: H2, O2, H2O
+
+2\. Ионная связь Электроны сгорают.
+
+\- Металлы и неметаллы
+
+Пример: NaCl, MgO
+
+3\. Металлическая связь
+
+\-Электроны в свободном движении.
+
+\- Среди металлов
+
+Пример: Fe, Cu, Al
+
+4\. Водородная связь.
+
+\- Слабая связь
+
+\- Между молекулами.
+
+\-Пример В воде, в ДНК', updated_at='2026-05-12 06:09:58' WHERE id=6;
+UPDATE lesson_translations SET title='Chemical bonding', content='# Methods of bond formation between atoms:
+
+1\. Covalent bond
+
+\-Electrons common
+
+\-Among non-metals
+
+Example: H2, O2, H2O
+
+2\. Ionic bonding - Electrons burn.
+
+\- Metals and non-metals
+
+Example: NaCl, MgO
+
+3\. Metal bonding
+
+\- Electrons in free motion
+
+\-Among metals
+
+Example: Fe, Cu, Al
+
+4\. Hydrogen bonding
+
+\- Weak link
+
+\- Between molecules
+
+\-Example In water, in DNA', updated_at='2026-05-12 06:03:57' WHERE id=7;
+UPDATE lesson_translations SET title='Valentlik', content='# Valentlik - atomning bog'' hosil qilish qobiliyati
+
+# Assosiy valentliklar:
+
+# I valentli: H,Na, K, Li,Cl, F.
+
+# II valentli: O, Ca, Mg, Zn, Ba
+
+# III valentli: Al, B, N, Fe
+
+# IV valentli: C,Si
+
+# Ko''p valentli elementlar
+
+# \- Fe II, III
+
+# \- Cu I,II
+
+# \- S II, IV, VI
+
+# \- N III, V
+
+# \- P III, V', updated_at='2026-05-12 06:16:24' WHERE id=9;
+UPDATE lesson_translations SET title='Валентность', content='# Валентность - способность атома образовывать связь.
+
+## Основные валентности:
+
+## 1-валентные: H, Na, K, Li, Cl, F. 2.
+
+## II валентные: O, Ca, Mg, Zn, Ba;
+
+## III валентность: Al, B, N, Fe
+
+## IV валентность: C,Si
+
+## Многовалентные элементы
+
+## \- Fe II, III;
+
+## \- Cu I, II
+
+## \- С II, IV, VI.
+
+## \- N III, V
+
+## \- Р III, V', updated_at='2026-05-12 06:17:17' WHERE id=10;
+UPDATE lesson_translations SET title='Valence', content='Valence is the ability of an atom to form a bond.
+
+Principal valences:
+
+I-valent: H, Na, K, Li, Cl, F.
+
+II-valent: O, Ca, Mg, Zn, Ba
+
+III-valent: Al, B, N, Fe
+
+IV-valent: C,Si
+
+Polyvalent elements
+
+\- Fe II, III
+
+\- Cu I, II
+
+\- C II, IV, VI.
+
+\- N III, V
+
+\- P III, V', updated_at='2026-05-12 06:18:33' WHERE id=11;
+UPDATE lesson_translations SET title='Oksidlanish darajasi', content='*   Oksidlanish darajasi - atomning shartli zaryadi.
+    
+*   Qoidalar:
+    
+*   Oddiy moddalarda: 0
+    
+*   H2, O2, Fe, Cu: 0
+    
+*   Ion birikmalarda = ion zaryadi:
+    
+*   Na +1, Cl -
+    
+*   Vododrod: +1 (metallar bilan -1)
+    
+*   Kislorod: -2 ( F bilan +2, peroksidlarda -1)
+    
+*   Birikmalarda yig''indi : 0 Misollar: H2O H (+1) O (-2) NH3 N (-3), (+1) H2SO4 H(+1) S (+6), ( -2) KMnO4 K(+1) Mn (+7) O (-2)', updated_at='2026-05-12 06:39:38' WHERE id=13;
+UPDATE lesson_translations SET title='Степень окисления', content='Степень окисления - условный заряд атома.
+
+Правила:
+
+В простых веществах: 0.
+
+H2, O2, Fe, Cu: 0;
+
+В ионных соединениях = ионный заряд:
+
+Na +1, Cl -1.
+
+Водород: +1 (с металлами -1)
+
+Кислород: -2 (в F +2, в пероксидах -1).
+
+Сумма в соединениях: Примеры: H2O H (+1) O (-2) NH3 N (-3), (+1) H2SO4 H (+1) S (+6), (-2)', updated_at='2026-05-12 06:42:59' WHERE id=14;
+UPDATE lesson_translations SET title='Oxidation state', content='Oxidation state is the conditional charge of an atom.
+
+Rules:
+
+In simple substances: 0
+
+H2, O2, Fe, Cu: 0.
+
+In ionic compounds = ionic charge:
+
+Na + 1, Cl - 1.
+
+Hydrogen: +1 (with metals -1)
+
+Oxygen: -2 (with F +2, in peroxides -1).
+
+Sum in compounds: 0 Examples: H2O H (+1) O (-2) NH3 N (-3), (+1) H2SO4 H (+1) S (+6), (-2)', updated_at='2026-05-12 06:42:28' WHERE id=15;
+UPDATE lesson_translations SET title='Kislotalar', content='Kislotalar - tarkibida metall atomlariga almashina oladigan vodorod atomlari va kislota qoldigʻidan iborat murakkab moddalar.
+
+Xossalari:
+
+Nordon tam
+
+Lakmus qog''ozini qizil rangga bo''yaydi
+
+Metallar bilan reakciyaga kirishadi
+
+Asoslar bilan neytrallanadi
+
+Kuchli kislotalar:
+
+\-HCl -xlorid kislota
+
+\-H2SO4 -sulfat kislota
+
+\-HNO3- nitrat kislota
+
+Kushsiz kislotalar:
+
+\-CH3COOH - sirka kislota
+
+\-H2CO3 - karbonat kislota
+
+\-H3PO4 - fosfat kislota', updated_at='2026-05-12 06:50:06' WHERE id=17;
+UPDATE lesson_translations SET title='Кислоты', content='Кислоты - это сложные вещества, состоящие из атомов водорода и кислотного остатка, которые могут быть замещены атомами металлов.
+
+Свойства:
+
+Кислый вкус
+
+Лакмус окрашивает бумагу в красный цвет.
+
+Реагирует с металлами.
+
+Нейтрализуется основаниями.
+
+Сильные кислоты:
+
+\-HCl -соляная кислота
+
+\-H2SO4 -серная кислота
+
+\-HNO3 - азотная кислота
+
+Слабые кислоты:
+
+\-CH3COOH - уксусная кислота;
+
+\-H2CO3 - углекислота;
+
+\-H3PO4 - фосфорная кислота;', updated_at='2026-05-12 06:50:53' WHERE id=18;
+UPDATE lesson_translations SET title='Acids', content='Acids are complex substances consisting of hydrogen atoms capable of being replaced by metal atoms and an acid residue.
+
+Properties:
+
+Sour taste
+
+Litmus dyes paper red
+
+Reacts with metals.
+
+Neutralized by bases
+
+Strong acids:
+
+\-HCl -chloric acid
+
+\-H2SO4 -sulfuric acid
+
+\-HNO3 - nitric acid
+
+Weak acids:
+
+\-CH3COOH - acetic acid
+
+\-H2CO3 - carbonic acid;
+
+\-H3PO4 - phosphoric acid;', updated_at='2026-05-12 06:51:27' WHERE id=19;
+UPDATE lesson_translations SET title='Asoslar', content='Asoslar - tarkibida metall atomi va bitta yoki bir nechta gidroksil (OH) guruhlari boʻlgan murakkab moddalar.
+
+Xossalari:
+
+Achchiq tam
+
+\-Lakmus qog''ozini ko''k rangga bo''yaydi
+
+\-Sovun kabi sirpanchiq
+
+\-Kislotalar bilan neytrallanadi
+
+Ishqorlar (eruvchi asoslar):
+
+\-NaOH -natriy gidroksid
+
+\-KOH- kaliy gidroksid
+
+\- Ca (OH)2 kalsiy gidroksid (ohak)
+
+\- Ba(OH)2 -bariy gidroksid
+
+Erimaydigan asoslar:
+
+\-Cu(OH)2-mis gidroksid
+
+\-Fe(OH)3- temir gidroksid
+
+\-Al(OH)3 -aluyminniy gidroksid', updated_at='2026-05-12 06:57:25' WHERE id=21;
+UPDATE lesson_translations SET title='Основания', content='Основания - сложные вещества, содержащие атом металла и одну или несколько гидроксильных (ОН) групп.
+
+Свойства:
+
+Горький вкус
+
+\- Лакмус окрашивает бумагу в синий цвет.
+
+\- Скользкая, как мыло.
+
+\- Нейтрализуется кислотами
+
+Щелочи (растворимые основания):
+
+\-NaOH -гидроксид натрия
+
+\-КОН-гидроксид калия
+
+\- Са (ОН) 2 гидроксид кальция (известь);
+
+\- Ва (ОН) 2 - гидроксид бария;
+
+Нерастворимые основания:
+
+\-Cu (OH) 2-гидроксид меди
+
+\-Fe (OH) 3-гидроксид железа
+
+\-Al (OH) 3 -гидроксид алюминия', updated_at='2026-05-12 06:59:03' WHERE id=22;
+UPDATE lesson_translations SET title='Bases', content='Bases are complex substances containing a metal atom and one or more hydroxyl (OH) groups.
+
+Properties:
+
+Bitter taste
+
+\- Litmus stains paper blue.
+
+\-Slippery as soap
+
+\- Neutralized by acids
+
+Alkali (soluble bases):
+
+\-NaOH - sodium hydroxide
+
+\-KOH - potassium hydroxide
+
+\- Ca (OH) 2 calcium hydroxide (lime)
+
+\- Ba (OH) 2 - barium hydroxide
+
+Insoluble bases:
+
+\-Cu (OH) 2 copper hydroxide
+
+\-Fe (OH) 3 - iron hydroxide
+
+\-Al (OH) 3 -aluminum hydroxide', updated_at='2026-05-12 06:58:27' WHERE id=23;
+UPDATE lesson_translations SET title='Tuzlar', content='## Tuzlar - tarkibi metall atomlari va kislota qoldiqlaridan iborat boʻlgan murakkab moddalar.
+
+## Tuzlar:
+
+## **O''rta tuzlar** To''liq neytral NaCL,CaCO3, KNO3
+
+## **Kislotali tuzlar** H atomlari qolgan NaHCO3, Ca(HCO3)2
+
+## **Asosli tuzlar** OH guruhlari qolgan MgOHCL, FeOHCL **Olish usullari:**
+
+## **1\. Metall+ kislota** Zn+H2SO4 = ZnSO4 + H2
+
+## **2\. Asos + kislota** NaOH + HCL
+
+## **3\. Oksid + oksid** CaO + CO2 = CaCO3
+
+## **4\. Tuz + tuz** AgNO3 + NaCL = AgCl + NaNO3', updated_at='2026-05-12 14:23:33' WHERE id=25;
+UPDATE lesson_translations SET title='Соли', content='Соли - сложные вещества, состоящие из атомов металлов и кислотных остатков.
+
+Соли:
+
+Средние соли Полностью нейтральные NaCl, CaCO3, KNO3.
+
+Кислые соли остальные атомы Н NaHCO3, Ca (HCO3) 2.
+
+Основные соли ОН-группы остальные MgOHCL, FeOHCL
+
+Способы получения:
+
+1\. Металл + кислота Zn + H2SO4 = ZnSO4 + H2.
+
+2\. Основание + кислота NaOH + НСІ.
+
+3\. Оксид + оксид СаО + СО2 = CaCO3.
+
+4\. Соль + соль AgNO3 + NaCl = AgCl + NaNO3.', updated_at='2026-05-12 14:23:33' WHERE id=26;
+UPDATE lesson_translations SET title='Salts', content='Salts are complex substances consisting of metal atoms and acid residues.
+
+Salts:
+
+Middle salts: Completely neutral NaCl, CaCO3, KNO3.
+
+Acid salts: NaHCO3, Ca (HCO3) 2, with the remaining H atoms.
+
+Basic salts with remaining OH groups MgOHCL, FeOHCL
+
+How to get:
+
+1\. Metal + acid Zn + H2SO4 = ZnSO4 + H2.
+
+2\. Base + acid NaOH + HCl.
+
+3\. Oxide + oxide CaO + CO2 = CaCO3.
+
+4\. Salt + salt AgNO3 + NaCl = AgCl + NaNO3.', updated_at='2026-05-12 14:24:34' WHERE id=27;
+UPDATE lesson_translations SET title='Oksidlar', content='## Oksidlar — ikki elementdan tashkil topgan, biri kislorod boʻlgan murakkab moddalar.
+
+## Oksidlar turlari:
+
+## Asosli oksidlar Na2O, CaO, FeO, CuO
+
+## Asoslar hosil qiladi CaO + H2O = Ca (OH)2
+
+## Kislotali oksidlar CO2, SO2, SO3, P2O5
+
+## kislotalar hosil qiladi SO3 + H2O = H2SO4
+
+## Amfoter oksidlar Al2O3, ZnO, Cr2O3 kislota va asos hossalari
+
+## inert oksidlar CO,NO, N2O reakciyaga kirmaslik', updated_at='2026-05-12 14:41:21' WHERE id=29;
+UPDATE lesson_translations SET title='Оксиды', content='Оксиды — это сложные вещества, состоящие из двух элементов, одним из которых является кислород.
+
+Типы оксидов:
+
+Основные оксиды Na2O, CaO, FeO, CuO.
+
+Образует основания CaO + H2O = Ca (OH) 2.
+
+Кислотные оксиды CO2, SO2, SO3, P2O5.
+
+образует кислоты SO3 + H2O = H2SO4
+
+Амфотерные оксиды Al2O3, ZnO, Cr2O3 свойства кислот и оснований.
+
+инертные оксиды CO,NO, N2O не вступать в реакцию', updated_at='2026-05-12 14:44:55' WHERE id=30;
+UPDATE lesson_translations SET title='Oxides', content='Oxides are complex substances consisting of two elements, one of which is oxygen.
+
+Types of oxides:
+
+Basic oxides Na2O, CaO, FeO, CuO
+
+CaO + H2O = Ca (OH) 2.
+
+Acidic oxides CO2, SO2, SO3, P2O5.
+
+forms acids SO3 + H2O = H2SO4
+
+Amphoteric oxides Al2O3, ZnO, Cr2O3, acidic and basic properties
+
+inert oxides CO,NO, N2O non-reaction', updated_at='2026-05-12 14:41:21' WHERE id=31;
+UPDATE lesson_translations SET title='Elektrolitik dissotsiatsiya', content='## Elektrolitik dissotsiatsiya — eritmada yoki suyuqlanmada moddalarning ionlarga parchalanish jarayoni.
+
+## Elektrolitlar:
+
+## Kuchli: HCl, H2SO4, NaOH, KOH, tuzlar
+
+## Kuchsiz: CH3COOH, H2CO3, NH4OH
+
+## Dissotsiatsiya tenglamalari:
+
+## HCL = H+ + CL-
+
+## NaOH = Na+ + OH -
+
+## Ion tenglamalar:
+
+## To''liq:
+
+## AgNO3 + NaCL = AgCl + NaNO3
+
+## qisqa
+
+## Ag + + NO3 - + Na + + CL- = AgCL + Na + + NO3 -
+
+## Ag + + CL- = AgCL', updated_at='2026-05-12 16:33:53' WHERE id=33;
+UPDATE lesson_translations SET title='Электролитическая диссоциация', content='Электролитическая диссоциация — процесс разложения веществ на ионы в растворе или расплаве.
+
+Электролиты:
+
+Сильные: HCl, H2SO4, NaOH, KOH, соли.
+
+Слабые: CH3COOH, H2CO3, NH4OH.
+
+Уравнения диссоциации:
+
+HCL = H+ + CL-;
+
+NaOH = Na+ + OH ̄.
+
+Ионные уравнения:
+
+Полное:
+
+AgNO3 + NaCl = AgCl + NaNO3.
+
+короткий
+
+Ag + + NO3 - + Na + + Cl- = AgCl + Na + + NO3 -
+
+Ag + Cl- = AgCl.', updated_at='2026-05-12 16:35:35' WHERE id=34;
+UPDATE lesson_translations SET title='Electrolytic dissociation', content='Electrolytic dissociation is the process of decomposing substances into ions in a solution or melt.
+
+Electrolytes:
+
+Strong: HCl, H2SO4, NaOH, KOH, salts.
+
+Weak: CH3COOH, H2CO3, NH4OH
+
+Dissociation equations:
+
+HCl = H+ + Cl-.
+
+NaOH = Na+ + OH−;
+
+Ion equations:
+
+Full:
+
+AgNO3 + NaCl = AgCl + NaNO3.
+
+brief
+
+Ag + + NO3 = AgCl + Na + + NO3 = AgCl + Na + + NO3 = AgCl + Na + + NO3.
+
+Ag + Cl2 = AgCl;', updated_at='2026-05-12 16:36:48' WHERE id=35;
